745ba336467dcb9df9e498e9bd32671148218420
Aditya Sharma <adityasharma972065@gmail.com>
2026-09-19 10:15:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (84.97%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        // Power of 2
+        if ((n & (n - 1)) != 0) {
+            return false;
+        }
+
+        // Power of 4
+        return (n - 1) % 3 == 0;
+    }
+}
