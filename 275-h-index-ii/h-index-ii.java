0a9617268367e533d3ad93f29a92d86689c96c7a
Aditya Sharma <adityasharma972065@gmail.com>
2026-09-22 12:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (46.49%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Number of papers having at least citations[mid] citations
+            int papers = n - mid;
+
+            if (citations[mid] >= papers) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return n - left;
+    }
+}
